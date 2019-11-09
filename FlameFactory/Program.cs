62a4450be2affdbeb10d9b61501f747906963b2e
Blekkspruter <jlm4508@rit.edu>
2019-11-09 18:06:38 -0500

    Init

---
+++ FlameFactory/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FlameFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
